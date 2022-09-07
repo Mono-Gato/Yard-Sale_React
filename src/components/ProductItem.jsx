4c23791ad2cb4766c99d0eeacca07ef86e87b759
Monogato <monogato.dg@gmail.com>
2022-09-07 13:53:42 -0500

    ADD ProductList Container and ProductItem component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import iconAddCart from '../assets/Icons/bt_add_to_cart.svg'
+import '../styles/ProductItem.scss'
+
+function ProductItem(){
+    return (
+        <div className="card">
+            <img src="https://images.pexels.com/photos/5465162/pexels-photo-5465162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card__image" />
+            <div className="card__info">
+                <div className="info__text">
+                    <p>$ 120,00</p>
+                    <p>Bmx</p>
+                </div>
+                <figure className="info__image">
+                    <img src={iconAddCart} alt />
+                </figure>
+            </div>
+        </div>
+    );
+};
+
+export { ProductItem };
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -4,6 +4,7 @@ import { Header } from '../components/Header'
 function Layout ({ children }) {
     return(
         <div className='Layout'>
+            <Header />
             { children }
         </div>
     )
